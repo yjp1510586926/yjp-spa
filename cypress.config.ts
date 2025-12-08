3d3ecbf3a77dea4afacbb29259142dffafbffc11
yjp <yangjinpeng@lawyeah.top>
2025-12-08 18:24:06 +0800

    init

---
+++ cypress.config.ts
@@ -0,0 +1,43 @@
+// import { defineConfig } from "cypress";
+
+// export default defineConfig({
+//   e2e: {
+//     baseUrl: "http://localhost:3000",
+//     supportFile: "./support/e2e.ts",
+//     specPattern: "./cypress/e2e/*.cy.ts",
+//   },
+
+//   component: {
+//     devServer: {
+//       framework: "react",
+//       bundler: "webpack",
+//     },
+//   },
+// });
+
+import { defineConfig } from "cypress";
+import webpackConfig from "./webpack.config";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+      // optionally pass in webpack config
+      webpackConfig,
+      // or a function - the result is merged with any
+      // webpack.config that is found
+      //   webpackConfig: async () => {
+      //     // ... do things ...
+      //     const modifiedConfig = await injectCustomConfig(baseConfig);
+      //     return modifiedConfig;
+      //   },
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "@/home";
 
 function helloWorld() {
-    console.log(Home());
-    return <div>hello world</div>
+	console.log(Home());
+	return <div>hello world</div>;
 }
-const root = createRoot(document.getElementById("app")!);
-if (root) {
-    root.render(helloWorld());
-} else {
-   throw new Error("root is null");
+const appElement = document.getElementById("app");
+if (!appElement) {
+	throw new Error("Failed to find the app element");
 }
+const root = createRoot(appElement);
+root.render(helloWorld());
