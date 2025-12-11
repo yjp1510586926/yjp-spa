553391ccfd03501110bb5a723081cceee5403b53
yjp <yjp1510586926@gmail.com>
2025-12-11 21:55:40 +0800

    chore: Apply consistent code formatting and linting across files.

--- cypress.config.ts
+++ cypress.config.ts
@@ -19,25 +19,25 @@ import { defineConfig } from "cypress";
 import webpackConfig from "./webpack.config";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
-      // optionally pass in webpack config
-      webpackConfig,
-      // or a function - the result is merged with any
-      // webpack.config that is found
-      //   webpackConfig: async () => {
-      //     // ... do things ...
-      //     const modifiedConfig = await injectCustomConfig(baseConfig);
-      //     return modifiedConfig;
-      //   },
-    },
-  },
+	component: {
+		devServer: {
+			framework: "react",
+			bundler: "webpack",
+			// optionally pass in webpack config
+			webpackConfig,
+			// or a function - the result is merged with any
+			// webpack.config that is found
+			//   webpackConfig: async () => {
+			//     // ... do things ...
+			//     const modifiedConfig = await injectCustomConfig(baseConfig);
+			//     return modifiedConfig;
+			//   },
+		},
+	},
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 });
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -14,23 +14,23 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
-import { mount } from 'cypress/react'
+import { mount } from "cypress/react";
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount
-    }
-  }
+	namespace Cypress {
+		interface Chainable {
+			mount: typeof mount;
+		}
+	}
 }
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add("mount", mount);
 
 // Example use:
 // cy.mount(<MyComponent />)
